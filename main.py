24a8f1d3c74d7b176bee836a7f3e8720cf5cf871
CannonBall Chris <57753280+cannonballchris@users.noreply.github.com>
2022-11-10 18:30:05 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+import discord
+from discord.ext import commands
+import json
+import os 
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('TOKEN')
+
+class SoloBot(commands.Bot):
+	def __init__(self):
+		intents = discord.Intents.all()
+		super().__init__(command_prefix='!', intents=intents)
+	
+	async def on_ready(self):
+		with open("config.json") as f:
+			config = json.load(f)
+		
+		setattr(self, "role", config["verification_role"])
+		print(f"Logged in as : {self.user.name}")
+
+bot = SoloBot()
+
+bot.load_extension("cogs.verify")
+bot.run(TOKEN)
+		
